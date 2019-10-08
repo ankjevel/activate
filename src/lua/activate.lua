eb6330bfa0f0ee1aa3cc17b0ac0e9812cb8a7a12
Dennis Pettersson <mail@dennispettersson.se>
2019-10-08 08:15:53 +0200

    fix: match actual env

--- src/lua/activate.lua
+++ src/lua/activate.lua
@@ -1,9 +1,23 @@
 local log = require 'log'
 local helpers = require 'helpers'
 
-local c_ok, client = helpers.client()
 local ip = ngx.var.remote_addr
+local blacklist = ngx.var.blacklist == '1'
+local localnet = ngx.var.localnet == '1'
+local allowed_country = ngx.var.allowed_country == '1'
 
+if blacklist or not allowed_country and not localnet then
+  log.err('bail on first ', ip, blacklist, localnet, allowed_country)
+  helpers.bail()
+  return
+end
+
+-- if connection coumes from local address, do nothing!
+if localnet then
+  return
+end
+
+local c_ok, client = helpers.client()
 if not c_ok then
   log.err('could not get client')
   helpers.bail()
